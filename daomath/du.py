4533ac869dc3f58f30b881d7a40671eddb4f2bd6
david <david.stankov@haemimont.com>
2019-05-16 17:43:50 +0300

    bulshit

--- daomath/du.py
+++ daomath/du.py
@@ -21,14 +21,14 @@ def solveODE(u, v, u0, v0, t0=0, h=0.1, n=100):
     y = v0
     t = t0
     for i in range(n):
-        k1 = h * u(x, y, t)
-        l1 = h * v(x, y, t)
+        k1 = h * u(t, x, y)
+        l1 = h * v(t, x, y)
         k2 = h * u(t + h / 2, x + k1 / 2, y + l1 / 2)
         l2 = h * v(t + h / 2, x + k1 / 2, y + l1 / 2)
         k3 = h * u(t + h / 2, x + k2 / 2, y + l2 / 2)
         l3 = h * v(t + h / 2, x + k2 / 2, y + l2 / 2)
-        k4 = h * u(t + h , x + k3 , y + l3 )
-        l4 = h * v(t + h , x + k3, y + l3 )
+        k4 = h * u(t + h, x + k3, y + l3)
+        l4 = h * v(t + h, x + k3, y + l3)
         k = (1 / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
         l = (1 / 6) * (l1 + 2 * l2 + 2 * l3 + l4)
         x = x + k
@@ -45,13 +45,63 @@ def solveODE(u, v, u0, v0, t0=0, h=0.1, n=100):
 
 def intergrate(x0, dxdt, t):
     x_args = [x0]
-    f=x0
+    t_args = [0]
+
+    f = x0
     for i in range(len(t) - 1):
         dt = t[i + 1] - t[i]
-        f = f+dxdt[i] * dt
+        f = f + dxdt[i] * dt
 
         x_args.append(f)
-    return np.array(x_args)
+        t_args.append(t)
+    return np.array([x_args, t]).T
 
 
+def derivate(x_args, t_args):
+    res = []
+    for i in range(len(x_args) - 1):
+        dfdt = (x_args[i + 1] - x_args[i]) / (t_args[i + 1] - t_args[i])
+        res.append(dfdt)
+    return np.array(res)
 
+
+def solve2Order(u_vx, v_vy, x0, y0, u0, v0, t0=0, h=0.1, n=10000):
+    y_args = [y0]  # y_args array for y arguments
+    x_args = [x0]  # x_args array for arguments
+    t_args = [t0]  # time interval
+    x_speed = [u0 ]
+    y_speed = [v0]
+    vx_next = u0
+    vy_next = v0
+    vx= u0
+    vy=v0
+    t = t0
+    x = x0
+    y = y0
+    z =h
+    h=h/2
+    for i in range(n):
+        k1 = h * u_vx(t, x, y)
+        l1 = h * v_vy(t, x, y)
+        k2 = h * u_vx(t + h / 2, x + k1 / 2, y + l1 / 2)
+        l2 = h * v_vy(t + h / 2, x + k1 / 2, y + l1 / 2)
+        k3 = h * u_vx(t + h / 2, x + k2 / 2, y + l2 / 2)
+        l3 = h * v_vy(t + h / 2, x + k2 / 2, y + l2 / 2)
+        k4 = h* u_vx(t + h, x + k3, y + l3)
+        l4 = h * v_vy(t + h, x + k3, y + l3)
+        k = (1 / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
+        l = (1 / 6) * (l1 + 2 * l2 + 2 * l3 + l4)
+        vx_next = vx_next + k
+        vy_next = vy_next + l
+        x_speed.append(vx)
+        y_speed.append(vy)
+        t = t + h
+        x = x + vx * z
+        y = y + vy * z
+        x_args.append(x)
+        y_args.append(y)
+        t_args.append(t)
+        vx = vx_next
+        vy=vy_next
+
+    return np.array([x_args, y_args, t_args]).T
