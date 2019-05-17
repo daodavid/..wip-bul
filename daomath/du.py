9dd4b4b5f73ca3b7529b49f7481acf9e9a6dffc7
daodeiv <daodeiv@Abv.bg>
2019-05-17 05:38:59 +0300

    # Runge Kutta method have been extending to solve system non liniear euations # adding animation polting example

--- daomath/du.py
+++ daomath/du.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as ani
 
 
 def solveODE(u, v, u0, v0, t0=0, h=0.1, n=100):
@@ -69,7 +71,7 @@ def solve2Order(u_vx, v_vy, x0, y0, u0, v0, t0=0, h=0.1, n=10000):
     y_args = [y0]  # y_args array for y arguments
     x_args = [x0]  # x_args array for arguments
     t_args = [t0]  # time interval
-    x_speed = [u0 ]
+    x_speed = [u0]
     y_speed = [v0]
     vx_next = u0
     vy_next = v0
@@ -78,9 +80,13 @@ def solve2Order(u_vx, v_vy, x0, y0, u0, v0, t0=0, h=0.1, n=10000):
     t = t0
     x = x0
     y = y0
-    z =h
-    h=h/2
     for i in range(n):
+        x = x+vx*h
+        y = y+vy*h
+        x_args.append(x)
+        y_args.append(y)
+        #plt.scatter(x,y)
+        #plt.show()
         k1 = h * u_vx(t, x, y)
         l1 = h * v_vy(t, x, y)
         k2 = h * u_vx(t + h / 2, x + k1 / 2, y + l1 / 2)
@@ -91,17 +97,60 @@ def solve2Order(u_vx, v_vy, x0, y0, u0, v0, t0=0, h=0.1, n=10000):
         l4 = h * v_vy(t + h, x + k3, y + l3)
         k = (1 / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
         l = (1 / 6) * (l1 + 2 * l2 + 2 * l3 + l4)
-        vx_next = vx_next + k
-        vy_next = vy_next + l
+
+        vx = vx + k
+        vy = vy + l
+        t = t + h
         x_speed.append(vx)
         y_speed.append(vy)
-        t = t + h
-        x = x + vx * z
-        y = y + vy * z
-        x_args.append(x)
-        y_args.append(y)
         t_args.append(t)
-        vx = vx_next
-        vy=vy_next
 
-    return np.array([x_args, y_args, t_args]).T
+
+    return np.array([x_args, y_args,x_speed,y_speed ,t_args]).T
+#
+# def leap_frog(v,u_0, x0, y0, u0, v0, t0=0, h=0.1, n=10000):
+#     x_n=x0
+#     y_n=y0
+#     Vx_n=u0
+#     Uy_n=v0
+#
+#     for i in range(n):
+#         x = v()
+
+#
+# fx = lambda t,x,y : -10*x/(np.sqrt(x**2+y**2))
+# fy = lambda t,x,y :-10*y/(np.sqrt(x**2+y**2))
+#
+# r = solve2Order(fx,fy,3,8,4,-10)
+# #plt.plot(r[:,4],r[:,3])
+# plt.plot(r[:,0],r[:,1])
+# plt.show()    tor figure
+
+
+
+
+k=100
+fx = lambda t,x,y :-k* x/(np.sqrt(x**2+y**2)*(x**2+y**2))
+fy = lambda t,x,y :-k* y/(np.sqrt(x**2+y**2)*(x**2+y**2))
+
+r = solve2Order(fx,fy,6,8,-4,-15)
+#plt.plot(r[:,4],r[:,3])
+plt.plot(r[:,0],r[:,1])
+
+def animate(i):
+    # fx = lambda t, x, y: -k * x / (np.sqrt(x ** 2 + y ** 2) * (x ** 2 + y ** 2))
+    # fy = lambda t, x, y: -k * y / (np.sqrt(x ** 2 + y ** 2) * (x ** 2 + y ** 2))
+    #r = solve2Order(fx, fy, 6, 8, -20, -15)
+
+    fx = lambda t, x, y: -10 * x / (np.sqrt(x ** 2 + y ** 2))
+    fy = lambda t,x,y :-10*y/(np.sqrt(x**2+y**2))
+    r = solve2Order(fx, fy, 3, 8, 4, -10)
+
+    plt.scatter(r[i,0],r[i,1])
+
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+anim = ani.FuncAnimation(fig,animate,interval=100)
+plt.show()
+
